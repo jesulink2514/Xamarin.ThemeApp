500d98ffb54d00f089a9c20a4ceef26ffb6254d4
Jesus Angulo <jesus.angulo@outlook.com>
2017-10-12 00:32:53 -0500

    Themes added

--- ThemeApp/ThemeApp/ViewModels/SettingsPageViewModel.cs
+++ ThemeApp/ThemeApp/ViewModels/SettingsPageViewModel.cs
@@ -3,14 +3,53 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
+using Prism.Navigation;
+using Xamarin.Forms;
+using Xamarin.Forms.Themes;
 
 namespace ThemeApp.ViewModels
 {
-    public class SettingsPageViewModel : BindableBase
+    public class SettingsPageViewModel : BindableBase, INavigatedAware
     {
+        private string _selectedTheme;
+
+        public List<string> Themes { get; private set; } = new List<string>()
+        {
+            "Light","Dark"
+        };
+
+        public string SelectedTheme
+        {
+            get => _selectedTheme;
+            set => SetProperty(ref _selectedTheme,value);
+        }
+
+        public ICommand ApplyCommand { get; private set; }
+
         public SettingsPageViewModel()
         {
+            ApplyCommand = new DelegateCommand(Apply);
+        }
 
+        private void Apply()
+        {
+            var theme = SelectedTheme == "Dark" ? typeof(DarkThemeResources) : typeof(LightThemeResources);
+            App.Current.Resources.MergedWith = theme;
+        }
+
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+            var x = typeof(DarkThemeResources);
+            var isDark = Application.Current.Resources.MergedWith == x;
+            SelectedTheme = isDark ? "Dark" : "Light";
         }
     }
 }
